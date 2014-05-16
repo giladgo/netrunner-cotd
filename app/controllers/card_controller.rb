9bf38f41d509f4caa66f3777c982f393c93082cb
giladgo <giladgo@gmail.com>
2014-05-16 12:47:30 +0300

    Adding ability to display a single card by code

--- app/controllers/card_controller.rb
+++ app/controllers/card_controller.rb
@@ -1,6 +1,11 @@
 class CardController < ApplicationController
   def cotd
     @card = Card.cotd
-    ap @card
+    render :card
+  end
+
+  def show
+    @card = Card.where(code: params[:code]).first
+    render :card
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 NetrunnerCotd::Application.routes.draw do
   get '/card/cotd'
+  get '/card/:code', to: 'card#show'
   get '/', to: redirect('/card/cotd')
 end
